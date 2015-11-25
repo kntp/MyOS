5e5029db966917f5bed5161ee29a6d17982f6604
Koji <kntp1972@gmail.com>
2015-11-25 23:02:24 +0900

    HelloWorldをgasプログラムで実装した

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
-os.img: ipl.o lnk.ls
-	ld -nostdlib -o os.img ipl.o -Tlnk.ls
+os.img: ipl.bin
+	mformat -f 1440 -C -B ipl.bin -i os.img
+
+ipl.bin: ipl.o lnk.ls
+	ld -nostdlib -o ipl.bin ipl.o -Tlnk.ls
 	
 ipl.o: ipl.s
 	as -o ipl.o ipl.s
 
 .PHONY: run
 run:
-	qemu-system-x86_64 -boot a -fda os.img
+	qemu -m 32 -localtime -vga std -fda os.img
 
 .PHONY: clean
 clean:
 	rm -f *~
-	rm -f ipl.img
+	rm -f ipl.bin
 	rm -f harios.sys
 	rm -f os.img
 	rm -f *.o
--- ipl.s
+++ ipl.s
@@ -1,43 +1,48 @@
 .code16
+.text
+	jmp entry
+	.byte 0x90
+	.ascii "HELLOIPL"	# name of boot sector
+	.word 512			# size per 1 sector
+	.byte 1				# cluster
+	.word 1				# start of FAT
+	.byte 2				# num of FAT
+	.word 224			# size of root directory
+	.word 2880			# drive size
+	.byte 0xf0			# media type
+	.word 9				# len of FAT area
+	.word 18			# secter num per track
+	.word 2				# head num
+	.int 0				# always "0"
+	.int 2880			# drive size
+	.byte 0, 0, 0x29
+	.int 0xffffffff		# volume serial number
+	.ascii "HELLO-OS   "	# disk name
+	.ascii "FAT12   "	# format name
+	.skip 18, 0x00		# 18 bytes space
 
-.globl _start
-_start:
-.byte 0xeb, 0x4e, 0x90
-.ascii "HELLOIPL"
-.word 512
-.byte 1
-.word 1
-.byte 2
-.word 224
-.word 2880
-.byte 0xf0
-.word 9
-.word 18
-.word 2
-.int 0
-.int 2880
-.byte 0, 0, 0x29
-.int 0xffffffff
-.ascii "HELLO-OS   "
-.ascii "FAT12   "
-.skip 18, 0x00
-
-.byte	0xb8, 0x00, 0x00, 0x8e, 0xd0, 0xbc, 0x00, 0x7c
-.byte	0x8e, 0xd8, 0x8e, 0xc0, 0xbe, 0x74, 0x7c, 0x8a
-.byte	0x04, 0x83, 0xc6, 0x01, 0x3c, 0x00, 0x74, 0x09
-.byte	0xb4, 0x0e, 0xbb, 0x0f, 0x00, 0xcd, 0x10, 0xeb
-.byte	0xee, 0xf4, 0xeb, 0xfd
-
-.byte 0x0a, 0x0a
-.ascii "hello, world"
-.byte 0x0a
-.byte 0
-
-.org 0x1fe, 0x00
-
-.byte 0x55, 0xaa
-
-.byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-.skip 4600, 0x00
-.byte 0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
-.skip 1469432, 0x00
+# main program
+entry:
+	movw $0, %ax		# init register
+	movw %ax, %ss
+	movw $0x7c00, %sp
+	movw %ax, %ds
+	movw %ax, %es
+	
+	movw $msg, %si
+putloop:
+	movb (%si), %al
+	addw $1, %si
+	cmpb $0, %al
+	je fin
+	movb $0x0e, %ah		# display one char
+	movw $15, %bx		# color code
+	int $0x10			# call video bios
+	jmp putloop
+fin:
+	hlt
+	jmp fin
+	
+.data
+msg:
+	.string "Hello, world!\n"
