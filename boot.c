eb95d983e6af5bae4a2b2d74176c4249679e1866
Koji <kntp1972@gmail.com>
2015-12-18 23:19:05 +0900

    マウスの３値取得

--- boot.c
+++ boot.c
@@ -56,6 +56,7 @@ void Main() {
 	struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	unsigned char mouse_dbuf[3], mouse_phase;
 
 	init_gdtidt();
 	init_pic();
@@ -78,6 +79,7 @@ void Main() {
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
 	enable_mouse();
+	mouse_phase = 0;
 
 	while(1) {
 		io_cli();
@@ -93,9 +95,28 @@ void Main() {
 			}else if(fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s, "%02X", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				if(mouse_phase == 0) {
+					/* waiting mouse 0xfa */
+					if(i == 0xfa) {
+						mouse_phase = 1;
+					}
+				}else if(mouse_phase == 1) {
+					/* waiting mouse 1st byte */
+					mouse_dbuf[0] = i;
+					mouse_phase = 2;
+				}else if(mouse_phase == 2) {
+					/* waiting mouse 2nd byte */
+					mouse_dbuf[1] = i;
+					mouse_phase = 3;
+				}else if(mouse_phase == 3) {
+					/* waiting mouse 3rd byte */
+					mouse_dbuf[2] = i;
+					mouse_phase = 1;
+					/* store 3bytes then display */
+					sprintf(s, "%02X %02X %02x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				}
 			}
 		}
 	}
